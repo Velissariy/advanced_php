caa97dae8ad40f1231985e47c0064f9890f1c507
Riddle <johnys2000@list.ru>
2024-10-13 15:28:31 +0500

    added Controllers

--- code/src/Controllers/UserController.php
+++ code/src/Controllers/UserController.php
@@ -5,15 +5,46 @@ namespace GB\App\Controllers;
 use GB\App\Render;
 use GB\App\Models\User;
 
-class UserController {
+class UsersController {
   public function actionIndex(): string {
     $users = User::getAllUsersFromStorage();
 
     $render = new Render();
     if (!$users) {
-      return $render->renderPage('user-empty.tpl', ['title' => "Список пользователей в хранилище", 'message' => "Список пуст или не найден."]);
+      return $render->renderPage('users-empty.twig', ['title' => "Список пользователей в хранилище", 'message' => "Список пуст."]);
     } else {
-      return $render->renderPage('users-index.tpl', ['title' => 'Список пользователей в хранилище', 'message' => $users]);
+      return $render->renderPage('users-index.twig', ['title' => 'Список пользователей в хранилище', 'users' => $users]);
+    }
+  }
+
+  public function actionSave(): string {
+    $name = $_GET['name'] ?? null;
+    $birthday = $_GET['birthday'] ?? null;
+
+    if (!$name) {
+      return "Ошибка: не указано имя пользователя.";
+    }
+
+    $user = new User($name);
+    if ($birthday) {
+      $user->setBirthdayFromString($birthday);
+    }
+
+    User::saveUser($user);
+    return "Пользователь добавлен.";
+  }
+
+  public function actionDelete(): string {
+    $name = $_GET['name'] ?? null;
+
+    if (!$name) {
+      return "Ошибка: не указан параметр name.";
+    }
+
+    if (User::deleteUser($name)) {
+      return "Пользователь удален.";
+    } else {
+      return "Ошибка: пользователь не найден.";
     }
   }
 }
